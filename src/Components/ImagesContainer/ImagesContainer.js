37edbb93a3e9f9a09524e5ec5ae8c110c9dc8b52
steffFrank <--replace-all>
2022-12-08 14:51:09 +0100

    feat: Display images

--- src/Components/ImagesContainer/ImagesContainer.js
+++ src/Components/ImagesContainer/ImagesContainer.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./imagesContainer.css";
 
-export const ImagesContainer = () => {
+export const ImagesContainer = ({photos}) => {
+    const displayPhotos = () => {
+        return photos.map(photo => {
+            return (
+                <img  src={photo.urls.regular} key={photo.id} alt={photo.description} title={photo.description} />
+            )
+        })
+    }
     return (
         <div className="img-container">
-
+            {displayPhotos()}
         </div>
     )
 }
