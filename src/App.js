6c7551a3aea81f861daa95126cc20b2bca2ac4bd
steffFrank <--replace-all>
2022-12-08 11:02:02 +0100

    feat: Add title and subtitle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 const App = () => {
-
+    return (
+        <header>
+            <h1>infinite scroll</h1>
+            <h2>images by unsplash</h2>
+        </header>
+    )
 }
 
 export default App;
